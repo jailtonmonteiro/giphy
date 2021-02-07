66904526ade9d83eb5ff5f438638550a28759781
Jailton Monteiro da Silva <jailtonmonteiro147@gmail.com>
2021-02-07 12:06:50 -0300

    Alteração para letra em caixa alta.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:giphy/ui/gif_page.dart';
+import 'package:Giphy/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
 import 'package:transparent_image/transparent_image.dart';
